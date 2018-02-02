ab1e580d5f5ca30f026a1ec7572f95f5c20a0df9
RagePu <233955487@qq.com>
2018-02-02 16:21:14 +0800

    vue-cli前台渲染安居客

---
+++ exercises/1.28-vue-cli、安居客改vue渲染/vue-cli前台渲染安居客/vue_test/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import router from './router'
+import cmpFixedTwo from '@/components/cmp-fixed-two'
+import axios from 'axios'
+
+Vue.config.productionTip = false
+Vue.prototype.$a = axios
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: {cmpFixedTwo},
+  template: '<div><router-view/><cmp-fixed-two/></div>'
+})
